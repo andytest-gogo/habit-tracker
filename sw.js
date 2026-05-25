6b7463fc58b681ac83851db359e70e3eb0a136cb
andytest-gogo <andy19920818@gmail.com>
2026-05-26 07:37:32 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'ht-v20260524-5';
+const CACHE_VERSION = 'ht-v20260526-1';
 
 self.addEventListener('install', event => {
   // Skip waiting immediately so new SW activates without waiting for old tabs to close
